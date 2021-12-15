8da8879d266861f3ceda6dde63fd7202226a1816
Eievui <14899090+GreenAndEievui@users.noreply.github.com>
2021-12-15 09:00:12 -0500

    Add string operations Use pointers to VArrays for reallocation functions; there is no reason to retain a dead pointer after it has been reallocated so this provides a cleaner interface.

--- c/varray/main.c
+++ c/varray/main.c
@@ -2,27 +2,38 @@
 #include "varray.h"
 
 int main() {
-    int* number_vector = va_new(10 * sizeof(int));
-    printf("Created VArray of size %zu\n", va_sizeof(number_vector));
-    for (int i = 0; i < va_sizeof(number_vector) / sizeof(int); i++) {
+    printf("Creating VArray of size %zu and filling it with multiples of 10.\n", va_size(number_vector));
+    int* number_vector = va_new(5 * sizeof(int));    
+    for (int i = 0; i < va_len(number_vector); i++)
         number_vector[i] = i * 10;
-        printf("Number %i is %i\n", i, number_vector[i]);
-    }
 
-    size_t old_size = va_sizeof(number_vector);
-    int* second_vector = va_expand(va_dup(number_vector), 10 * sizeof(int));
-    puts("Expanding vector...");
-    for (int i = old_size / sizeof(int); i < va_sizeof(second_vector) / sizeof(int); i++) {
+    printf("Expanding VArray to %zu and appending multiples of 16.\n", va_size(second_vector));
+    int* second_vector = va_dup(number_vector);
+    va_expand(&second_vector, 3 * sizeof(int));
+    for (int i = va_len(number_vector); i < va_len(second_vector); i++)
         second_vector[i] = i * 16;
-    }
-    printf("After expansion, VArray has a size of %zu.\n", va_sizeof(second_vector));
 
     puts("The final VArray contains:");
-    for (int i = 0; i < va_sizeof(second_vector) / sizeof(int); i++) {
+    for (int i = 0; i < va_len(second_vector); i++)
         printf("va[%i]:\t%i\n", i, second_vector[i]);
-    }
 
-    puts("Done. Exiting.");
-    va_free(&number_vector);
-    va_free(&second_vector);
+    va_free(number_vector);
+    va_free(second_vector);
+
+    puts("Done with number tests. Now testing string operations.");
+    puts("Creating two strings, \"Hello\" and \"World\", adding punctuation, and concatenating them.");
+    char* mutable_string = va_newstr("Hello");
+    va_append_char(&mutable_string, ',');
+    va_append_char(&mutable_string, ' ');
+
+    char* another_string = va_newstr("World");
+    va_append_char(&another_string, '!');
+    
+    va_strcat(&mutable_string, another_string);
+
+    puts(mutable_string);
+    puts(another_string);
+
+    va_free(mutable_string);
+    va_free(another_string);
 }
--- c/varray/varray.h
+++ c/varray/varray.h
@@ -1,7 +1,7 @@
 #pragma once
 
-// I've said before that one of the only reasons I use C++ is for std::vector
-// and std::unordered_map. This tiny library aims to fix that.
+// I've said before that one of the only reasons I use C++ is for std::string,
+// std::vector and std::unordered_map. This tiny library aims to fix that.
 
 // This library has been made as lightweight as possible. There is no compiled
 // code, just a collection of macros and static inline functions.
@@ -9,21 +9,29 @@
 // Ultimately this library is able to produce very small and fast output while
 // abstracting away the logic behind variably-sized arrays.
 
-// The "V" in "VArray" can be interpreted as either "varible-size" or "vector",
+// The "V" in "VArray" can be interpreted as either "variable" or "vector",
 // whichever you prefer to read it as :)
 
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define _va_head(va) ((VArrayHeader*) ((va) - sizeof(VArrayHeader)))
+// Return the header of a VArray.
+#define va_header(va) ((VArrayHeader*) (va) - 1)
+// Get the number of elements in a VArray. Only works if the type is known.
+#define va_len(va) (va_size(va) / sizeof(typeof(*va)))
+// Append a value of any type to the end of a VArray.
+#define va_append(va, i) \
+    (va_expand((&va), sizeof(i))), \
+    ((va)[va_len(va) - 1] = (i))
 
-// A malloc-like interface for accessing a dynamically-sized array.
+// Header for VArray objects. This is placed *before* the array, so that the
+// user is able to directly index the array and use any type of their choosing.
 typedef struct VArrayHeader {
     size_t size; // Current size of the array in bytes.
 } VArrayHeader;
 
-// Constuct a new VArray of a given size.
+// Construct a new VArray of a given size.
 static inline void* va_new(size_t s) {
     VArrayHeader* head = malloc(s + sizeof(VArrayHeader));
     head->size = s;
@@ -31,30 +39,55 @@ static inline void* va_new(size_t s) {
 }
 
 // Get the size of a VArray.
-static inline size_t va_sizeof(void* va) {
-    return _va_head(va)->size;
+static inline size_t va_size(void* va) {
+    return va_header(va)->size;
 }
 
-// Resizes a VArray.
-static inline void* va_resize(void* va, size_t s) {
-    VArrayHeader* head = realloc(_va_head(va), sizeof(VArrayHeader) + s);
+// Resizes a VArray. Requires a pointer to the VArray.
+static inline void va_resize(void* va, size_t s) {
+    VArrayHeader* head = realloc(va_header(*(void**) va), sizeof(VArrayHeader) + s);
     head->size = s;
-    return head + 1;
+    *(void**) va = head + 1;
 }
 
-// Appends additional bytes to a VArray.
-static inline void* va_expand(void* va, size_t s) {
-    return va_resize(va, _va_head(va)->size + s);
+// Appends additional bytes to a VArray. Requires a pointer to the VArray.
+static inline void va_expand(void* va, size_t s) {
+    va_resize(va, va_header(*(void**) va)->size + s);
 }
 
 // Duplicate a VArray.
 static inline void* va_dup(void* va) {
-    VArrayHeader* new_va = malloc(_va_head(va)->size + sizeof(VArrayHeader));
-    memcpy(new_va, _va_head(va), _va_head(va)->size + sizeof(VArrayHeader));
+    VArrayHeader* new_va = malloc(va_header(va)->size + sizeof(VArrayHeader));
+    memcpy(new_va, va_header(va), va_header(va)->size + sizeof(VArrayHeader));
     return new_va + 1;
 }
 
 // Free a VArray's buffer.
 static inline void va_free(void* va) {
-    free(_va_head(va));
+    free(va_header(va));
+}
+
+// Create a new VArray using a string.
+static inline void* va_newstr(const char* str) {
+    size_t string_len = strlen(str);
+    void* result = va_new(string_len);
+    memcpy(result, str, string_len);
+    return result;
+}
+
+// Append a single byte to a VArray string and adjust the null byte. Requires
+// a pointer to the VArray.
+static inline void va_append_char(char** va, char c) {
+    va_expand(va, sizeof(char));
+    (*va)[va_size(*va) - 1] = c;
+    (*va)[va_size(*va)] = 0;
+}
+
+// Append a string to the end of a VArray. Requires a pointer to the VArray.
+static inline void va_strcat(char** va, const char* str) {
+    size_t s = strlen(str);
+    size_t old_size = va_size(*va);
+    va_expand(va, s);
+    memcpy(*va + old_size, str, s);
+    va_header(*va)->size += s;
 }
